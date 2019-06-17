93464bfc2242c6a8d2080f31d680b0363cc8bc16
MageshDominator <mageshwaran951@gmail.com>
2019-06-17 21:49:45 +0530

    FLANN and template matching

--- modifiedFLANNAlgo.py
+++ modifiedFLANNAlgo.py
@@ -8,6 +8,8 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+import json
+from tqdm import tqdm
 
 base_dir = os.getcwd()
 data_folder = os.path.join(base_dir, "Dataset")
@@ -16,6 +18,7 @@ images_folder = os.path.join(data_folder, "Images")
 crops_folder = os.path.join(data_folder, "Crops")
 sample_testset = os.path.join(data_folder, "sample_testset")
 
+sample_json_result = os.path.join(sample_testset, "sample_result.json")
 sample_images = os.path.join(sample_testset, "images")
 sample_crops = os.path.join(sample_testset, "crops")
 
@@ -32,9 +35,23 @@ def ModifiedFLANN(img1, img2):
     kp2, des2 = sift.detectAndCompute(img2, None)
     
     orgBorder = None
+    flannMatch = True
     
     if (des1 is None) or (des2 is None):
-        return orgBorder
+        flannMatch = False
+        
+        if (img2.shape[0] > img1.shape[0]) and (img2[1].shape[1] > img1.shape[1]):
+            res = cv2.matchTemplate(img2,img1, cv2.TM_CCOEFF)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+            h, w, c = img1.shape
+            
+            pts = [int(min_loc[0]), int(min_loc[0]) + w,
+                   int(max_loc[0]), int(max_loc[1])]
+            
+            return flannMatch, pts
+
+        return flannMatch, orgBorder
+    
     flann = cv2.FlannBasedMatcher(index_params, search_params)
     matches = flann.knnMatch(des1, des2, k=2)
     
@@ -58,37 +75,33 @@ def ModifiedFLANN(img1, img2):
         
         H, status = cv2.findHomography(cropImg, orgImg, cv2.RANSAC, 3.0)
 
+        if H is None:
+            return flannMatch, orgBorder
+        
         h, w, c = img1.shape
         
         cropBorder = np.float32([[[0,0], [0,h-1], [w-1,h-1], [w-1,0]]])
+        
         orgBorder = cv2.perspectiveTransform(cropBorder, H)
         
         cv2.polylines(img2, [np.int32(orgBorder)], True, (0, 255, 0), 5)
-    
-    else:
-        print("No Good Matches Found")
-    
-    matched = cv2.cvtColor(img2, cv2.COLOR_BGR2RGB)
-    plt.imshow(matched)
-    plt.show()
-    
-    return orgBorder
+   
+    return flannMatch, orgBorder
 
 def findMinMax(border):
     x, y = np.transpose(border)[0], np.transpose(border)[1]
     
-    x1, x2 = x.min(), x.max()
+    x1, x2 = int(x.min()), int(x.max())
     
-    y1, y2 = y.min(), x.max()
+    y1, y2 = int(y.min()), int(x.max())
     
-    return (x1, y1, x2, y2)
+    return [x1, y1, x2, y2]
 
 completeTracker = {}
 
-allImages = os.listdir(sample_images)
-allcropImages = os.listdir(sample_crops)
+noAssociationCropImages = os.listdir(sample_crops)
 
-for imagefile in os.listdir(sample_images):
+for imagefile in tqdm(os.listdir(sample_images)):
     img = cv2.imread(os.path.join(sample_images,
                                    imagefile))
     imageTracker = []
@@ -97,20 +110,31 @@ for imagefile in os.listdir(sample_images):
         crop_img = cv2.imread(os.path.join(sample_crops,
                                            cropfile))
 
-        crop_border = ModifiedFLANN(crop_img, img)
-    
-        if crop_border is None:
-            print("Images are Not associated")
+        flannMatch, crop_border = ModifiedFLANN(crop_img, img)
         
+        if flannMatch:
+            if crop_border is not None:
+                pts = findMinMax(crop_border[0])
+                imageTracker.append((cropfile.replace(".jpg", ""), pts))
+                if cropfile in noAssociationCropImages:
+                    noAssociationCropImages.remove(cropfile)
         else:
-            print("images are Associated")
-            pts = findMinMax(crop_border[0])
-            imageTracker.append((cropfile.replace(".jpg", ""), pts))
+            if crop_border is not None:
+                imageTracker.append((cropfile.replace(".jpg", ""), crop_border))
+                if cropfile in noAssociationCropImages:
+                    noAssociationCropImages.remove(cropfile)
     
     completeTracker[imagefile.replace(".jpg", "")] = imageTracker
-# =============================================================================
-# plt.imshow(cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB))
-# plt.show()
-# 
-# =============================================================================
 
+NA_Crops = []
+
+for crop in noAssociationCropImages:
+    NA_Crops.append([crop.replace(".jpg", ""), []])
+    
+completeTracker["NA"] = NA_Crops
+
+with open(sample_json_result, "w") as f:
+    json.dump(completeTracker, f)
+
+
+print("Output Json File is generated")
